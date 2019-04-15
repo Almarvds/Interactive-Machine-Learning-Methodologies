c6377126e332ae4cb2c7bad969b46b046854c7fa
Almarvds <a.r.v.d.stappen@student.tue.nl>
2019-04-15 16:58:09 +0200

    Basic interactions

--- src/test_class.java
+++ src/test_class.java
@@ -19,6 +19,9 @@ public class test_class extends PApplet
     int blocksY;
     int distanceBetweenBlocks;
 
+    //code elements
+    boolean[] enabled = new boolean[0];
+
     public static void main(String[]args){
         PApplet.main("test_class",args);
     }
@@ -46,31 +49,47 @@ public class test_class extends PApplet
         noStroke();
         fill(240,240,240);
         rect(largeBlockX,largeBlockY,largeBlockSize,largeBlockSize,10);
+
+        enabled = new boolean[numberofBlocks];
     }
 
     public void draw(){
         for(int i=0;i<numberofBlocks;i++){
             drawCategory(blocksX,blocksY+((sizeofBlocks+distanceBetweenBlocks)*i));
+            if(enabled[i] == true){
+                drawX(i);
+            }
         }
+        drawLikeandDislike();
 
-        //check mouse for hovering items and clicking them
-        if(mouseX>sizeofBlocks && mouseX<sizeofBlocks*2){
-            for(int i=0;i<numberofBlocks;i++){
-                if(mouseY>blocksY+((sizeofBlocks+distanceBetweenBlocks)*i) && mouseY<blocksY+ sizeofBlocks +
-                        ((sizeofBlocks+distanceBetweenBlocks)*i)){
+        //check mouse for hovering categories
+        if(mouseX>sizeofBlocks && mouseX<sizeofBlocks*2) {
+            for (int i = 0; i < numberofBlocks; i++) {
+                if (mouseY > blocksY + ((sizeofBlocks + distanceBetweenBlocks) * i) && mouseY < blocksY + sizeofBlocks +
+                        ((sizeofBlocks + distanceBetweenBlocks) * i)) {
                     hoverCategory(i);
                 }
             }
         }
-    }
 
+        //check mouse for hovering the like and dislike
+        if(mouseY>(largeBlockY+largeBlockSize+20)/2 && mouseY < (largeBlockY+largeBlockSize+20)/2+40){
+            fill(200);
+            if(mouseX>largeBlockX+20 && mouseX < largeBlockX + 60){
+                rect(largeBlockX+20,(largeBlockY+largeBlockSize+20)/2,40,40);
+            }
+            if(mouseX>largeBlockX+largeBlockSize-60 && mouseX < largeBlockX+largeBlockSize-20){
+                rect(largeBlockX+largeBlockSize-60,(largeBlockY+largeBlockSize+20)/2,40,40);
+            }
+        }
+    }
 
     public void mousePressed(){
         if(mouseX>sizeofBlocks && mouseX<sizeofBlocks*2){
             for(int i=0;i<numberofBlocks;i++){
                 if(mouseY>blocksY+((sizeofBlocks+distanceBetweenBlocks)*i) && mouseY<blocksY+ sizeofBlocks +
                         ((sizeofBlocks+distanceBetweenBlocks)*i)){
-                    disableCategory(i);
+                    clickedCategory(i);
                 }
             }
         }
@@ -83,20 +102,34 @@ public class test_class extends PApplet
         rect(a,b,sizeofBlocks,sizeofBlocks,10);
     }
 
-
     //draws hover effects
     void hoverCategory(int i){
         fill(255,255,255,200);
         rect(blocksX,blocksY+((sizeofBlocks+distanceBetweenBlocks)*i),sizeofBlocks,sizeofBlocks,10);
     }
 
-
-    //disables categories
-    void disableCategory(int i){
+    void drawX(int i){
         PImage redX = loadImage("C:\\Users\\s159536\\IdeaProjects\\Furnishing_App\\Assets\\images\\buttons\\redX.png");
-        image(redX,blocksX,blocksY+((sizeofBlocks+distanceBetweenBlocks)*i),sizeofBlocks,sizeofBlocks);
-        System.out.println("button "+ i + " clicked");
+        image(redX,blocksX+10,blocksY+((sizeofBlocks+distanceBetweenBlocks)*i)+10,sizeofBlocks-20,sizeofBlocks-20);
     }
 
+    //disables or enables categories when clicked
+    void clickedCategory(int i){
+        enabled[i] =! enabled[i];
+    }
+
+    void drawLikeandDislike(){
+        fill(0);
+        rect(largeBlockX+20,(largeBlockY+largeBlockSize+20)/2,40,40);
+        rect(largeBlockX+largeBlockSize-60,(largeBlockY+largeBlockSize+20)/2,40,40);
+    }
+
+    void likeButtonPressed(){
+
+    }
+
+    void dislikeButtonPressed(){
+
+    }
 
 }
